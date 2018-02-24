613d6b8655f16271c749ef314ff935b3b64ffa83
Spencer Manzon <samanzon@ucalgary.ca>
2018-02-24 15:47:17 -0700

    pre-alpha version
    basic working foundation

---
+++ public/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    var socket = io();
+    $('form').submit(function(){
+        socket.emit('chat', $('#m').val());
+        $('#m').val('');
+        return false;
+    });
+    socket.on('chat', function(msg){
+        $('#messages').append($('<li>').text(msg));
+    });
+});
